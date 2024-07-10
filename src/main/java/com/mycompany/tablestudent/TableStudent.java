db0075ca0803b2c9c714b7b9b238dd5e8718fab6
IgnasZuklys <111701610+IgnasZuklys@users.noreply.github.com>
2024-07-10 19:10:11 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/tablestudent/TableStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tablestudent;
+
+/**
+ *
+ * @author makal
+ */
+public class TableStudent {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        tableCool mainPage = new tableCool();
+        //tableDate nextPage = new tableDate();
+        
+        
+        //mainPage.setNextStage(nextPage);
+        
+        
+        mainPage.setVisible(true);
+    }
+}
